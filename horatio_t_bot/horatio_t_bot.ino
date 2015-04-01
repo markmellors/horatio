040b646032b42b4a6985d5ad33c1f1061a3d913b
markmellors <mark.mellors@gmail.com>
2015-04-01 08:42:45 +0100

    all timings and movements refined - working

--- horatio_t_bot/horatio_t_bot.ino
+++ horatio_t_bot/horatio_t_bot.ino
@@ -4,7 +4,7 @@
 // Ultrasonic detection stuff
 const int TRIGGER_PIN = 3;
 const int ECHO_PIN = 4;
-const int MAX_DISTANCE = 50;
+const int MAX_DISTANCE = 150;
 
 // Instance of ultrasonic detector
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
@@ -19,7 +19,7 @@ const int NUMBER_OF_WAVES = 3;
 // for left & right side of wave
 int waveDeflect = 150;
 // Time to allow for each wave (ms)
-int waveTime = 1000;
+int waveTime = 850;
 // home point for wave servo (us)
 const int WAVE_HOME = 1500;
 // centre point for wave action (us)
@@ -28,19 +28,20 @@ const int WAVE_CENTRE = 1500;
 const int NUMBER_OF_SHAKES = 3;
 // microseconds of deflection from SHAKE_HOME
 // for up & down portion of shake
-int shakeDeflect = 200;
+int shakeDeflect = 225;
 // Time to allow for each wave (ms)
-int shakeTime = 1000;
+int shakeTime = 700;
 // home point for wave servo (us)
-const int SHAKE_HOME = 700;
+const int SHAKE_HOME = 650;
 // centre point for wave action (us)
-const int SHAKE_CENTRE = 900;
-
+const int SHAKE_CENTRE = 925;
+const int SHAKE_CENTRE_FOR_WAVE = 1600;
 // home point for wrist servo (us)
-const int WRIST_HOME = 1600;
+const int WRIST_HOME = 1465;   //600=palm down 1000=thumb right, 1465=thumb and little figner level, 1500=thumb left 2000=palm up  2200=palm right 2600=thumb down
 // centre point for wave action (us)
 const int WRIST_CENTRE = 1600;
-
+const int WRIST_CENTRE_FOR_WAVE = 1420;
+const int WRIST_CENTRE_FOR_SHAKE = 1600;
 //servo definitions
 const int wristServoPin = 1;
 const int shakeServoPin = 2;
@@ -58,7 +59,7 @@ void setup() {
   //wait on power up, so that we can unplug the programmign cable and plug in the power supply. we don't want to try to run 3 servos with a 1A stall each from a usb socket do we...
   delay(3000);
   
-    Serial.begin(115200);
+//    Serial.begin(115200);
     waveServo.attach(waveServoPin);
     shakeServo.attach(shakeServoPin);
     wristServo.attach(wristServoPin);
@@ -69,17 +70,16 @@ void loop() {
     delay(50);
     int distance = getDistance();
     bool doThings = shouldDoThings(distance);
-
     // If we have something in range,
     // and we're not already doing so, wave;
     if(doThings){
-    int waveOrShake = 10; //random(100);
+        int waveOrShake = random(100);
         if (waveOrShake > 50) {
             shake(distance);
-            Serial.println("Shake hands");
+    //        Serial.println("Shake hands");
         } else {
             wave(distance);
-            Serial.println("waving");
+  //          Serial.println("waving");
         }
         delay(1000);
         goHome();
@@ -122,6 +122,7 @@ int getDistance(){
 }
 
 void goHome(){
+  //home is lying flat on desk, palm down
     waveServo.writeMicroseconds(WAVE_HOME);
     shakeServo.writeMicroseconds(SHAKE_HOME);
     wristServo.writeMicroseconds(WRIST_HOME);
@@ -129,7 +130,14 @@ void goHome(){
 }
 
 void wave(int uS){
+
     isRunning = true;
+    //raise the hand for the start of the wave
+    shakeServo.writeMicroseconds(SHAKE_CENTRE_FOR_WAVE);
+    //rotate the hand for the start of the wave
+    wristServo.writeMicroseconds(WRIST_CENTRE_FOR_WAVE);
+    
+    delay(2000);
     for (int n=0; n < NUMBER_OF_WAVES; n++){
         waveServo.writeMicroseconds(WAVE_CENTRE + waveDeflect);
         delay(waveTime);
@@ -143,6 +151,11 @@ void wave(int uS){
 
 void shake(int uS){
     isRunning = true;
+    //move hand and rotate the wrist ready for the shake
+    shakeServo.writeMicroseconds(SHAKE_CENTRE);
+    wristServo.writeMicroseconds(WRIST_CENTRE_FOR_SHAKE);
+    delay(400);
+    
     for (int n=0; n < NUMBER_OF_SHAKES; n++){
         shakeServo.writeMicroseconds(SHAKE_CENTRE - shakeDeflect);
         delay(shakeTime);
